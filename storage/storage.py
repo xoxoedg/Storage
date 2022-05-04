522fb8be1e5d507a1ed306bf4e35ccdb2a0ba12c
xoxoedg <rugyasin@gmail.com>
2022-05-04 22:08:19 +0200

    Initial Commit

---
+++ storage/storage.py
@@ -0,0 +1,53 @@
+from pyModbusTCP.client import ModbusClient
+
+from storage.utils.converter import PowerConverter
+
+
+class Storage:
+
+    def __init__(self, IP, grid_locations, name):
+        self.name = name
+        self.grid_locations = grid_locations
+        self.IP = IP
+        self.client = ModbusClient(host=IP, port=502)
+        self.power_change_limit = 250
+
+    def initialize_client(self):
+        self.client.open()
+
+    def get_data_single_register(self, register):
+        register_value = self.client.read_input_registers(register, 1)
+        return register_value[0] if register is not None else "Register is empty"
+
+    def get_data_multiple_register(self, register):
+        register_value = self.client.read_input_registers(register, 2)
+        if register_value[0] > 32767:
+            register_value[0] -= (2 * 32768)
+            int_val = (register_value[0] * 65536 + register_value[1]) / 1000
+            return int_val
+        return register_value[0]
+
+    def send_power(self, power, register):
+        power_in_mw = PowerConverter.convert_to_mw(power)
+        current_power_in_mw = PowerConverter.convert_to_mw(self.get_data_multiple_register(register))
+        if abs(current_power_in_mw - power_in_mw) > PowerConverter.convert_to_mw(self.power_change_limit):
+            power = current_power_in_mw + 100 * power_in_mw / abs(power_in_mw)
+        if power < 0:
+            power_in_mw += 4294967296
+        return power_in_mw
+
+    def write_data_to_storage(self, power, register, should_send_to_storage):
+        power_to_send = self.send_power(power, register)
+        if should_send_to_storage:
+            self.client.write_multiple_registers(2000, [power_to_send // 65536, power_to_send % 65536])
+        else:
+            print("Data not send")
+
+
+
+
+
+
+
+
+
---
+++ storage/utils/converter.py
@@ -0,0 +1,5 @@
+class PowerConverter:
+
+    @staticmethod
+    def convert_to_mw(power):
+        return int(power * 1000)
